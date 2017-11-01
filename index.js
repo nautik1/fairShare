6213d2cca184e9182b61f9c1d0f76edd0186cea2
Nicolas P <nicolas@n-p.fr>
2017-11-01 19:40:58 +0100

    Replaced not working module with scrapegoat fork

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-var caldav = require("node-caldav-mod");
+var Scrapegoat = require("scrapegoat");
 var moment = require("moment");
 var params = require("./params.json");
 
-const dateTimeFormat = "YYYYMMDDTHHmmss[Z]";
+const scrapegoat = new Scrapegoat(params);
+
+const dateTimeFormat = "YYYYMMDD[T]HHmmss[Z]";
 
 var isoStart = moment("2017-09-01", "YYYY-MM-DD").format(dateTimeFormat);//toISOString().replace(/[-.:]/g, "");
 var isoEnd = moment("2017-10-01", "YYYY-MM-DD").format(dateTimeFormat);//toISOString().replace(/[-.:]/g, "");
 
 console.log(isoStart);
 
-caldav.getEvents(params.url, params.user, params.pass, isoStart, isoEnd, (data) => {
+scrapegoat.getEventsByTime(isoStart, isoEnd).then((data) => {
   console.log("done");
   console.log(data);
 });
